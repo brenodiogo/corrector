fcf2cf2ed65960a6b8899c362b8034cb0bf60838
Breno Diogo de Carvalho Camargos <breno.camargos@ana.gov.br>
2022-02-23 19:30:51 -0300

    Initial commit

---
+++ src/main/java/com/brenocamargos/corrector/CorrectorApplication.java
@@ -0,0 +1,13 @@
+package com.brenocamargos.corrector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorrectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorrectorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/brenocamargos/corrector/CorrectorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.brenocamargos.corrector;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorrectorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
